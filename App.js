543cf0fc6bf909444fbdb3340962cf66bbc4e215
Thaer Alsarakbi <thaer@innov8tif.com>
2022-10-10 11:08:55 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { StyleSheet, View, Text, StatusBar } from 'react-native';
+
+export default function App() {
+
+  return (
+    <View>
+      <Text>Result</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
